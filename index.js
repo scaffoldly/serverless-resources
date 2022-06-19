c1b2d4d856a2e9a5f69a67dd44c285ca9d389c93
Christian Nuss <christian.nuss@gmail.com>
2022-06-19 16:12:10 -0400

    initialize function

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 "use strict";
 
 class ServerlessResources {
-  constructor() {
-    // The plugin is loaded
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.hooks = {
+      initialize: () => this.init(),
+    };
+  }
+
+  init() {
+    console.log("Serverless instance: ", this.serverless);
+
+    // `serverless.service` contains the (resolved) serverless.yml config
+    const service = this.serverless.service;
+    console.log("Provider name: ", service.provider.name);
+    console.log("Functions: ", service.functions);
   }
 }
 
